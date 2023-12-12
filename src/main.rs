af26ab4e23abc745597f2d3339c812cb23179359
Arto Gahr <artogahr@gmail.com>
2023-12-12 22:19:19 +0100

    Implement inital cli parsing using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{arg, Command};
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("aragog")
+        .version("0.1")
+        .author("Arto")
+        .about("Crawls a link up to the depth of N recursions and saves them to local files")
+        .arg(arg!(--url <VALUE>).short('u').required(true))
+        .arg(
+            arg!(--depth <VALUE>)
+                .short('n')
+                .default_value("2")
+                .required(false),
+        )
+        .get_matches();
+
+    println!(
+        "URL: {:?}",
+        matches
+            .get_one::<String>("url")
+            .expect("Initial URL is required")
+    );
+    println!("Depth: {:?}", matches.get_one::<String>("url").unwrap());
 }
