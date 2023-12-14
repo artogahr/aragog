36db4bedb65ede9c771fa42ae73fa1e0ff9e60bb
Arto Gahr <artogahr@gmail.com>
2023-12-14 21:29:02 +0100

    MVP

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::{arg, Command};
+use std::io::Write;
+use webpage::{Webpage, WebpageOptions};
 fn main() {
     let matches = Command::new("aragog")
         .version("0.1")
@@ -13,11 +15,42 @@ fn main() {
         )
         .get_matches();
 
-    println!(
-        "URL: {:?}",
-        matches
-            .get_one::<String>("url")
-            .expect("Initial URL is required")
-    );
-    println!("Depth: {:?}", matches.get_one::<String>("url").unwrap());
+    let url = matches.get_one::<String>("url").unwrap();
+
+    dbg!(url);
+
+    let depth = matches
+        .get_one::<String>("depth")
+        .unwrap()
+        .parse::<u32>()
+        .unwrap();
+
+    work_website(url, depth);
+}
+
+fn work_website(url: &String, depth: u32) {
+    dbg!(url);
+    let info = Webpage::from_url(url, WebpageOptions::default());
+    let mut path = std::env::current_dir().unwrap();
+    path.push("output");
+    match Result::from(info) {
+        Ok(info) => {
+            path.push(info.html.title.unwrap());
+            std::fs::create_dir_all(&path).unwrap();
+            path.push("index.html");
+            let mut file = std::fs::File::create(path).unwrap();
+            file.write_all(info.html.text_content.as_bytes()).unwrap();
+            if depth > 0 {
+                for link in info.html.links {
+                    //check if the link is an url
+                    if link.url.starts_with("http") {
+                        work_website(&link.url, depth - 1);
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
 }
