3342f1a50552bc5636c35589a3a1d4a012e5b853
Arto Gahr <artogahr@gmail.com>
2023-12-17 19:15:38 +0100

    fix recursion, don't recurse into links with same domain as current one

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{arg, Command};
 use std::io::Write;
+use url::Url;
 use webpage::{Webpage, WebpageOptions};
 fn main() {
     let matches = Command::new("aragog")
@@ -32,14 +33,26 @@ fn main() {
 
 fn work_website(url: &String, depth: u32, depth_remaining: u32, all_links: &mut Vec<String>) {
     all_links.push(url.to_string());
-    for i in 0..depth {
-        println!("{}{}", " ".repeat(i as usize), url);
-    }
+    println!("{}  {}", " ".repeat(depth as usize), url);
     let info = Webpage::from_url(url, WebpageOptions::default());
     let mut path = std::env::current_dir().unwrap();
     path.push("output");
     match Result::from(info) {
         Ok(info) => {
+            //recurse through the links
+            if depth_remaining > 0 {
+                let current_domain = Url::parse(url).unwrap().domain().unwrap().to_string();
+                for link in info.html.links {
+                    //check if the link is an URL and has a different domain
+                    if link.url.starts_with("http") {
+                        let link_domain =
+                            Url::parse(&link.url).unwrap().domain().unwrap().to_string();
+                        if link_domain != current_domain && !all_links.contains(&link.url) {
+                            work_website(&link.url, depth + 1, depth_remaining - 1, all_links);
+                        }
+                    }
+                }
+            }
             //create a directory for the page
             match info.html.title {
                 Some(title) => {
@@ -53,15 +66,6 @@ fn work_website(url: &String, depth: u32, depth_remaining: u32, all_links: &mut 
             path.push("index.html");
             let mut file = std::fs::File::create(path).unwrap();
             file.write_all(info.html.text_content.as_bytes()).unwrap();
-            //recurse through the links
-            if depth_remaining > 0 {
-                for link in info.html.links {
-                    //check if the link is an url
-                    if link.url.starts_with("http") && !all_links.contains(&link.url) {
-                        work_website(&link.url, depth + 1, depth_remaining - 1, all_links);
-                    }
-                }
-            }
         }
         Err(e) => {
             println!("{}", e);
